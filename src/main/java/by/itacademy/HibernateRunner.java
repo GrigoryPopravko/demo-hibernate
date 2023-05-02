9d3cd0fe70f33d1af84cd13ae7f92b1247000bef
GrigoryPopravko <gregory.popravko@pluto.tv>
2023-05-02 17:44:06 +0200

    HibernateSession

--- src/main/java/by/itacademy/HibernateRunner.java
+++ src/main/java/by/itacademy/HibernateRunner.java
@@ -1,33 +1,50 @@
 package by.itacademy;
 
-import by.itacademy.entity.AuthorEntity;
 import by.itacademy.entity.BookEntity;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.time.LocalTime;
+
 public class HibernateRunner {
+
+    private static final Logger logger = LogManager.getLogger(HibernateRunner.class);
+
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
         try (SessionFactory sessionFactory = configuration.configure().buildSessionFactory()) {
+            LocalTime start = LocalTime.now();
             Session session = sessionFactory.openSession();
+            logger.error("Открыл сессию");
             Transaction transaction = session.beginTransaction();
-//            session.persist();
-//            session.merge();
-//            session.remove();
-//            session.get();
 
+            BookEntity book = session.get(BookEntity.class, 300L);
+            logger.warn("Достал книжку");
+            if (book != null) {
+                logger.debug("Достаю тайтл");
+                System.out.println(book.getTitle());
+                logger.debug("Достал тайтл: " + book.getTitle());
+            } else {
+                logger.debug("Книжки нет достаю другую");
+                book = session.get(BookEntity.class, 6L);
+                logger.trace("Достал тайтл: " + book.getTitle());
+            }
 
-            session.persist(BookEntity.builder()
-                    .title("Punishment")
-                    .pages(300)
-                    .build());
-            session.persist(AuthorEntity.builder()
-                    .fullName("Fedor Dostoevskii")
-                    .build());
 
             transaction.commit();
+            System.out.println("Закрыл транзакцию");
+
+            //get
+            //persist
+            //merge
+            //remove
+
+            LocalTime end = LocalTime.now();
+            System.out.println("Время выполнения: " + (end.getNano() - start.getNano()));
         }
     }
 }
